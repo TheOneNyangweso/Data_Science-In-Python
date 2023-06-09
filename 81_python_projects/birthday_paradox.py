10def831a478da6145eded6c4239e2d963ce0066
TheOneNyangweso <morurisammy5@gmail.com>
2023-06-09 06:34:31 +0300

    "pandas"

---
+++ 81_python_projects/birthday_paradox.py
@@ -0,0 +1,17 @@
+"""
+The birthday paradox - an example of a Monte Carlo experiment
+"""
+import datetime
+import random
+
+def get_birthdays(no_of_bds):
+    """
+    Return a list of random birthdays
+    """
+    pass
+def get_match(birthdays):
+    """
+    Return the date object of a birthday that has occured more than once
+    """
+    pass
+
