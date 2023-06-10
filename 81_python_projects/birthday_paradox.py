6aa287a2bf65b58059471760c5e39eceb48a27ce
TheOneNyangweso <morurisammy5@gmail.com>
2023-06-10 21:43:13 +0300

    "pandas"

--- 81_python_projects/birthday_paradox.py
+++ 81_python_projects/birthday_paradox.py
@@ -4,6 +4,8 @@ The birthday paradox - an example of a Monte Carlo experiment
 import datetime
 import random
 
+MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+
 def get_birthdays(no_of_bds):
     """
     Return a list of random birthdays
@@ -15,3 +17,10 @@ def get_match(birthdays):
     """
     pass
 
+while True:
+    print('How many birthdays should I generate? (Max 100)')
+    response = int(input('>>> '))
+    if response.isdecimal() and (0 < response <= 100):
+        num_of_birthdays_gen = response
+        break
+    
